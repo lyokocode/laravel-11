0522f9c92bf23166f8a0574b1b3687291ed18c2f
lyokocode <aelita.dev@gmail.com>
2024-09-28 04:55:12 +0300

    login page UI

--- resources/views/pages/Client/Login/index.blade.php
+++ resources/views/pages/Client/Login/index.blade.php
@@ -1,3 +1,64 @@
-<x-client.layout>
-    hello form login page
-</x-client.layout>
+<!DOCTYPE html>
+<html lang="tr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>/AELITA || Giriş Yap</title>
+    @vite('resources/css/app.css')
+
+</head>
+
+<body>
+
+    {{-- page container --}}
+    <div class="flex items-center justify-center bg-gray-300 w-full h-screen">
+        {{-- wrapper --}}
+        <div class="text-center bg-gray-200 w-1/3 min-w-80 max-w-xl rounded-md p-3">
+            <h1 class="text-3xl font-bold text-gray-700">/AELITA</h1>
+            <h2 class="my-2 text-lg font-medium text-gray-600">Yeni bir hesap oluştur.</h2>
+
+            <form action="{{ route('login') }}" method="post" class="text-center flex flex-col items-center gap-y-2">
+
+                @csrf
+
+                {{-- email --}}
+                <div class="flex flex-col w-2/3 items-start ">
+                    <label for="email" class="text-md font-semibold text-gray-600">Email:</label>
+                    <input type="text" name="email" value="{{ old('email') }}"
+                        placeholder="@error('email') {{ $message }} @enderror"
+                        class="w-full rounded-md pl-3 font-semibold text-gray-700 p-1 placeholder:text-red-500 placeholder:text-sm placeholder:text-left placeholder:font-light
+                        placeholder:pl-0 text-left @error('email') border border-red-500 @enderror">
+                </div>
+
+                {{-- password --}}
+                <div class="flex flex-col w-2/3 items-start ">
+                    <label for="password" class="text-md font-semibold text-gray-600">Şifre:</label>
+                    <input type="password" name="password"
+                        placeholder="@error('password') {{ $message }} @enderror"
+                        class="w-full rounded-md pl-3 font-semibold text-gray-700 p-1 placeholder:text-red-500 placeholder:text-sm placeholder:text-left placeholder:font-light
+                        placeholder:pl-0 text-left @error('password') border border-red-500 @enderror">
+                </div>
+
+                {{-- remember me --}}
+                <div class="w-2/3 text-start">
+                    <input type="checkbox">
+                    <label>Beni Hatırla</label>
+                </div>
+
+                <button class="bg-sky-500 text-sky-100 font-semibold text-xl px-2 py-1 rounded-md">giriş yap</button>
+
+                {{-- route to login page --}}
+                <div class="self-end text-end">
+                    <h3>henüz bir hesabın yok mu?</h3>
+                    <a href="{{ route('login') }}" class='text-blue-600'>kayıt ol</a>
+                </div>
+            </form>
+
+        </div>
+    </div>
+
+</body>
+
+</html>
