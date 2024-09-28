e6bc71d2126b7afe6c3b759cddb5d68737331d08
lyokocode <aelita.dev@gmail.com>
2024-09-28 05:52:44 +0300

    how to use group middleware for user session in laravel 11

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,24 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
 
+
+Route::middleware('auth')->group(function(){
+
+    Route::get('/profilim',[DashboardController::class,'index'])->name('profile');
+
+    Route::post('/cikis-yap', [AuthController::class, 'logout'])->name('logout');
+
+});
+
+Route::middleware('guest')->group(function(){
+    Route::view('/giris-yap','pages/Client/Login.index')->name('login');
+    Route::view('/kayit-ol','pages/Client/Register.index')->name('register');
+    
+    Route::post('/kayit-ol', [AuthController::class, 'register']);
+    Route::post('/giris-yap', [AuthController::class, 'login']);
+
+});
+
 Route::view('/', 'pages/Client/Home.index')->name('home');
 
 Route::view('/bloglar', 'pages/Client/Blogs.index')->name('blogs');
@@ -14,14 +32,9 @@ Route::view('/projeler','pages/Client/Projects.index')->name('projects');
 
 Route::view('/soru-cevap','pages/Client/Explore.index')->name('topic');
 
-Route::view('/giris-yap','pages/Client/Login.index')->name('login');
-
-Route::view('/kayit-ol','pages/Client/Register.index')->name('register');
-
-Route::get('/profilim',[DashboardController::class,'index'])->name('profile');
 
 
-Route::post('/kayit-ol', [AuthController::class, 'register']);
-Route::post('/giris-yap', [AuthController::class, 'login']);
-Route::post('/cikis-yap', [AuthController::class, 'logout'])->name('logout');
+
+
+
 
