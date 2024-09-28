9dfa3316d77fcd5f04493ee982100e77644770aa
lyokocode <aelita.dev@gmail.com>
2024-09-28 03:39:02 +0300

    sidebar UI

--- resources/views/components/Client/Sidebar/index.blade.php
+++ resources/views/components/Client/Sidebar/index.blade.php
@@ -1,3 +1,26 @@
-<article class="h-[92vh] border-r border-r-gray-400 sticky top-[8vh]">
-    sidebar
+<article
+    class="h-[92vh] border-r border-r-gray-400 sticky top-[8vh] flex flex-col items-center justify-center gap-y-2 px-3 py-2">
+
+    <div class="flex flex-col text-center gap-y-1">
+        <a href=""
+            class="bg-[#adadad] px-3 py-1 rounded-lg text-xl font-semibold hover:bg-gray-400 duration-500">anasayfa</a>
+        <a href="" class="px-3 py-1 rounded-lg text-xl font-semibold hover:bg-gray-400 duration-500">bloglar</a>
+        <a href="" class="px-3 py-1 rounded-lg text-xl font-semibold hover:bg-gray-400 duration-500">Keşfet</a>
+        <a href="" class="px-3 py-1 rounded-lg text-xl font-semibold hover:bg-gray-400 duration-500">Keşfet</a>
+        <a href="" class="px-3 py-1 rounded-lg text-xl font-semibold hover:bg-gray-400 duration-500">Soru &
+            Cevap</a>
+    </div>
+
+    <hr class="border-2 w-full border-[#c0c0c2]">
+
+    <h1 class="w-full text-left underline text-2xl">Kategoriler</h1>
+
+    <div class="flex flex-col text-center gap-y-1">
+        <a href="" class="px-3 py-1 rounded-lg text-lg font-medium hover:bg-zinc-400 duration-500">JavaScript</a>
+        <a href="" class="px-3 py-1 rounded-lg text-lg font-medium hover:bg-zinc-400 duration-500">React</a>
+        <a href="" class="px-3 py-1 rounded-lg text-lg font-medium hover:bg-zinc-400 duration-500">Express.JS</a>
+        <a href="" class="px-3 py-1 rounded-lg text-lg font-medium hover:bg-zinc-400 duration-500">Laravel</a>
+        <a href="" class="px-3 py-1 rounded-lg text-lg font-medium hover:bg-zinc-400 duration-500">PostgreSQL</a>
+        <a href="" class="px-3 py-1 rounded-lg text-lg font-medium hover:bg-zinc-400 duration-500">Sequelize</a>
+    </div>
 </article>
